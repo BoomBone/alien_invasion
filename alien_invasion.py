f206f46a55fc0f8dd70f916b902311d4304fee4a
BoomBone <ticccccc@163.com>
2018-01-13 07:22:16 +0800

    init project

---
+++ alien_invasion.py
@@ -0,0 +1,23 @@
+import pygame
+import sys
+
+from settings import Settings
+
+
+def run_game():
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height)
+    )
+    screen.fill(ai_settings.bg_color)
+    pygame.display.set_caption("Alien Invasion")
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+        pygame.display.flip()
+
+
+run_game()
---
+++ settings.py
@@ -0,0 +1,5 @@
+class Settings():
+    def __init__(self):
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
