385e6f039ac313b94a167f42512a50f25c577cd6
BoomBone <ticccccc@163.com>
2018-01-13 16:14:41 +0800

    限制飞船的活动范围

--- ship.py
+++ ship.py
@@ -26,9 +26,9 @@ class Ship():
     def update(self):
         """根据移动标志调整飞船的位置"""
         # 更新飞船的center值而不是rect
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.center += self.ai_settings.ship_speed_factory
-        if self.moving_left:
+        if self.moving_left and self.rect.left > 0:
             self.center -= self.ai_settings.ship_speed_factory
 
         # 根据self.center更新rect对象
