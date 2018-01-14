7d1565eca260cfc48e2c5b5e71df14beb140f6ce
BoomBone <ticccccc@163.com>
2018-01-14 10:33:06 +0800

    创建Bullet类

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-#外星人入侵游戏
+# 外星人入侵游戏
 - alien_invasion.py
 - settings.py
 - game_functions.py
--- settings.py
+++ settings.py
@@ -4,3 +4,8 @@ class Settings():
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
         self.ship_speed_factory = 1.5
+        # 子弹设置
+        self.bullet_speed_factory = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60
