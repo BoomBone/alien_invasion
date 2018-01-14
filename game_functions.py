2b6900333730f18d056b7ce1eb9a3cdb00a4c654
BoomBone <ticccccc@163.com>
2018-01-14 23:20:33 +0800

    重构create_fleet()

--- game_functions.py
+++ game_functions.py
@@ -9,17 +9,11 @@ def create_fleet(ai_settings, screen, aliens):
     # 创建一个外星人，并计算一行可以容纳多少个外星人
     # 外星人间距为外星人宽度
     alien = Alien(ai_settings, screen)
-    alien_width = alien.rect.width
-    available_space_x = ai_settings.screen_width - 2 * alien_width
-    number_aliens_x = int(available_space_x / (2 * alien_width))
+    number_aliens_x = get_number_aliens_x(ai_settings, alien.rect.width)
 
     # 创建第一行外星人
     for alien_number in range(number_aliens_x):
-        # 创建一个外星人并将其加入当前行
-        alien = Alien(ai_settings, screen)
-        alien.x = alien_width + 2 * alien_width * alien_number
-        alien.rect.x = alien.x
-        aliens.add(alien)
+        create_alien(ai_settings, screen, aliens, alien_number)
 
 
 def check_keydown_events(event, ai_settings, screen, ship, bullets):
@@ -82,3 +76,19 @@ def fire_bullet(ai_settings, screen, ship, bullets):
     if len(bullets) < ai_settings.bullets_allowed:
         new_bullets = Bullet(ai_settings, screen, ship)
         bullets.add(new_bullets)
+
+
+def get_number_aliens_x(ai_settings, alien_width):
+    """计算每行可容纳多少个外星人"""
+    available_space_x = ai_settings.screen_width - 2 * alien_width
+    number_aliens_x = int(available_space_x / (2 * alien_width))
+    return number_aliens_x
+
+
+def create_alien(ai_settings, screen, aliens, alien_number):
+    """创建一个外星人并将其放在当前行"""
+    alien = Alien(ai_settings, screen)
+    alien_width = alien.rect.width
+    alien.x = alien_width + 2 * alien_width * alien_number
+    alien.rect.x = alien.x
+    aliens.add(alien)
