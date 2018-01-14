3cc0cae764cccb79bd80a043f610d59144e58af4
BoomBone <ticccccc@163.com>
2018-01-14 19:55:34 +0800

    开火

--- game_functions.py
+++ game_functions.py
@@ -1,25 +1,18 @@
 import sys
 import pygame
+from bullet import Bullet
 
 
-def check_events(ship):
-    """响应按键和鼠标时间"""
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
-        elif event.type == pygame.KEYDOWN:
-            check_keydown_events(event, ship)
-
-        elif event.type == pygame.KEYUP:
-            check_keyup_events(event, ship)
-
-
-def check_keydown_events(event, ship):
+def check_keydown_events(event, ai_settings, screen, ship, bullets):
     """响应按键"""
     if event.key == pygame.K_RIGHT:
         ship.moving_right = True
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
+    elif event.key == pygame.K_SPACE:
+        # 创建一颗子弹，并将其加入到编组bullets中
+        new_bullets = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullets)
 
 
 def check_keyup_events(event, ship):
@@ -29,7 +22,22 @@ def check_keyup_events(event, ship):
         ship.moving_left = False
 
 
-def update_screeen(ai_settings, screen, ship):
+def check_events(ai_settings, screen, ship, bullets):
+    """响应按键和鼠标时间"""
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            check_keydown_events(event, ai_settings, screen, ship, bullets)
+
+        elif event.type == pygame.KEYUP:
+            check_keyup_events(event, ship)
+
+
+def update_screeen(ai_settings, screen, ship, bullets):
     """更新屏幕上的图像，并切换到新屏幕"""
     screen.fill(ai_settings.bg_color)
+    # 在飞船和外星人后面重绘所有子弹
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
     ship.blitme()
