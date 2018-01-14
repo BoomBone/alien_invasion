ea1f87a1b98757c51e82c506a3c2a3b761c79c75
BoomBone <ticccccc@163.com>
2018-01-14 23:05:31 +0800

    创建外星人群

--- alien_invasion.py
+++ alien_invasion.py
@@ -15,12 +15,12 @@ def run_game():
         (ai_settings.screen_width, ai_settings.screen_height)
     )
     pygame.display.set_caption("Alien Invasion")
-    # 创建一艘飞船
+    # 创建一艘飞船、一个外星人、一个子弹编组和一个外星人编组
     ship = Ship(ai_settings, screen)
-    # 创建一个外星人
-    alien = Alien(ai_settings, screen)
-    # 创建一个用于存储子弹的编组
     bullets = Group()
+    aliens = Group()
+    # 创建外星人群
+    gf.create_fleet(ai_settings, screen, aliens)
 
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
@@ -28,6 +28,7 @@ def run_game():
         ship.update()
         gf.upadte_bullets(bullets)
         # print(len(bullets))
-        gf.update_screen(ai_settings, screen, ship, alien, bullets)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+
 
 run_game()
--- game_functions.py
+++ game_functions.py
@@ -1,6 +1,25 @@
 import sys
 import pygame
 from bullet import Bullet
+from alien import Alien
+
+
+def create_fleet(ai_settings, screen, aliens):
+    """创建外星人"""
+    # 创建一个外星人，并计算一行可以容纳多少个外星人
+    # 外星人间距为外星人宽度
+    alien = Alien(ai_settings, screen)
+    alien_width = alien.rect.width
+    available_space_x = ai_settings.screen_width - 2 * alien_width
+    number_aliens_x = int(available_space_x / (2 * alien_width))
+
+    # 创建第一行外星人
+    for alien_number in range(number_aliens_x):
+        # 创建一个外星人并将其加入当前行
+        alien = Alien(ai_settings, screen)
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        aliens.add(alien)
 
 
 def check_keydown_events(event, ai_settings, screen, ship, bullets):
@@ -34,17 +53,18 @@ def check_events(ai_settings, screen, ship, bullets):
             check_keyup_events(event, ship)
 
 
-def update_screen(ai_settings, screen, ship, alien, bullets):
+def update_screen(ai_settings, screen, ship, aliens, bullets):
     """更新屏幕上的图像，并切换到新屏幕"""
     screen.fill(ai_settings.bg_color)
     # 在飞船和外星人后面重绘所有子弹
     for bullet in bullets.sprites():
         bullet.draw_bullet()
     ship.blitme()
-    alien.blitme()
+    aliens.draw(screen)
 
     # 让最近绘制的屏幕可见
     pygame.display.flip()
+
 
 def upadte_bullets(bullets):
     """更新子弹的位置，并删除已经消失的子弹"""
