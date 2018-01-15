579d0c553cd0b2ad23ddcd217ec9abb488a0f0ee
Ting <ticccccc@163.com>
2018-01-15 10:39:47 +0800

    提高子弹速度

--- settings.py
+++ settings.py
@@ -5,7 +5,7 @@ class Settings():
         self.bg_color = (230, 230, 230)
         self.ship_speed_factory = 1.5
         # 子弹设置
-        self.bullet_speed_factory = 1
+        self.bullet_speed_factory = 3
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60
