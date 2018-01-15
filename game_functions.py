c934fc95a4eda7dc7adfdca8bc63d5d26d48b59b
Ting <ticccccc@163.com>
2018-01-15 13:21:43 +0800

    游戏结束

--- game_functions.py
+++ game_functions.py
@@ -150,19 +150,19 @@ def check_fleet_edges(ai_settings, aliens):
 
 def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
     """响应被外星人撞到飞船"""
-    # 将ships_left减1
-    stats.ships_left -= 1
-
-    # 清空外星人列表和子弹列表
-    aliens.empty()
-    bullets.empty
-
-    # 创建一群新的外星人，并将飞船放到屏幕底端中央
-    create_fleet(ai_settings, screen, ship, aliens)
-    ship.center_ship()
-
-    # 暂停
-    sleep(0.5)
+    if stats.ships_left > 0:
+        # 将ships_left减1
+        stats.ships_left -= 1
+        # 清空外星人列表和子弹列表
+        aliens.empty()
+        bullets.empty
+        # 创建一群新的外星人，并将飞船放到屏幕底端中央
+        create_fleet(ai_settings, screen, ship, aliens)
+        ship.center_ship()
+        # 暂停
+        sleep(0.5)
+    else:
+        stats.game_active = False
 
 
 def check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets):
