3f2137ebd6e5be2e9d345c3a8e504f3f0729b905
Ting <ticccccc@163.com>
2018-01-15 10:57:23 +0800

    检测外星人和飞船碰撞

--- game_functions.py
+++ game_functions.py
@@ -117,13 +117,16 @@ def get_number_rows(ai_settings, ship_height, alien_height):
     return number_rows
 
 
-def update_aliens(ai_settings, aliens):
+def update_aliens(ai_settings, ship, aliens):
     """
     更新外星人群中所有外星人的位置
     检查是否有外星人位于屏幕边缘，并更新整群外星人的位置
     """
     check_fleet_edges(ai_settings, aliens)
     aliens.update()
+    # 检测外星人和飞船之间的碰撞
+    if pygame.sprite.spritecollideany(ship, aliens):
+        print("Ship hit!!!")
 
 
 def change_fleet_direction(ai_settings, aliens):
