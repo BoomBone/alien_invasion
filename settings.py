18074613a643e151fb7bb3efc3968eb453e52492
Ting <ticccccc@163.com>
2018-01-15 09:16:15 +0800

    创建表示外星人移动方向的设置

--- settings.py
+++ settings.py
@@ -12,5 +12,6 @@ class Settings():
         self.bullets_allowed = 3
         # 外星人设置
         self.alien_speed_factor = 1
-
-
+        self.fleet_drop_speed = 10
+        # fleet_direction为1表示向右移，为-1表示向左移
+        self.fleet_direction = 1
