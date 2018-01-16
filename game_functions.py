f8fca95f5235fddfb1b892ca48fb1428751b78a3
Ting <ticccccc@163.com>
2018-01-16 10:37:38 +0800

    将Play按钮切换到非活动状态

--- game_functions.py
+++ game_functions.py
@@ -43,7 +43,8 @@ def check_events(ai_settings, screen, stats, play_button, ship, aliens,
 def check_play_button(ai_settings, screen, stats, play_button, ship, aliens,
                       bullets, mouse_x, mouse_y):
     """在玩家单机Play按钮时开始新游戏"""
-    if play_button.rect.collidepoint(mouse_x, mouse_y):
+    button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
+    if button_clicked and not stats.game_active:
         # 重置游戏信息
         stats.reset_stats()
         stats.game_active = True
