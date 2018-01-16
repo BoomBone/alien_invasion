7f232ac3b01e5a721bccc026ccb74572d6ea5229
Ting <ticccccc@163.com>
2018-01-16 13:45:57 +0800

    将消灭的每个外星人得点数都计入得分

--- game_functions.py
+++ game_functions.py
@@ -100,8 +100,9 @@ def check_bullet_alien_collisions(ai_settings, screen, stats, sb, ship,
     collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
 
     if collisions:
-        stats.score += ai_settings.alien_points
-        sb.prep_score()
+        for aliens in collisions.values():
+            stats.score += ai_settings.alien_points * len(aliens)
+            sb.prep_score()
 
     if len(aliens) == 0:
         # 删除现有的子弹，加快游戏节奏，并新建一群外星人
