312d8c646d8d5f3ac5e098a0adb3635298231c5e
Ting <ticccccc@163.com>
2018-01-16 13:53:46 +0800

    提高点数

--- settings.py
+++ settings.py
@@ -20,6 +20,8 @@ class Settings():
         self.fleet_direction = 1
         # 以什么样的速度加快游戏节奏
         self.speedup_scale = 1.1
+        # 外星人点数的提高速度
+        self.score_scale = 1.5
 
         self.initialize_dynamic_settings()
 
@@ -34,7 +36,10 @@ class Settings():
         self.alien_points = 50
 
     def increase_speed(self):
-        """提高速度设置"""
+        """提高速度设置和外星人的点数"""
         self.ship_speed_factor *= self.speedup_scale
         self.bullet_speed_factor *= self.speedup_scale
         self.alien_speed_factor *= self.speedup_scale
+
+        self.alien_points = int(self.alien_points * self.score_scale)
+        print(self.alien_points)
